aae1dc0cceb85c01165a4b4f96ad41e11eb7f1d6
sathya <sathyaprakash777.sp@gmail.com>
2021-03-08 11:53:02 +0530

    third commit

--- learncrypto/__init__.py
+++ learncrypto/__init__.py
@@ -19,8 +19,7 @@ def create_app(config_class=Config):
     app.config.from_object(Config)
 
     db.init_app(app)
-    with app.app_context():
-         db.create_all()
+    app.app_context().push()
     bcrypt.init_app(app)
     login_manager.init_app(app)
     mail.init_app(app)
