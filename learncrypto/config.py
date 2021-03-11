26076d23d0621a580796c555ccbb354b9ce8ba25
sathya <sathyaprakash777.sp@gmail.com>
2021-03-11 21:18:09 +0530

    config3

--- learncrypto/config.py
+++ learncrypto/config.py
@@ -2,7 +2,7 @@ import os
 
 
 class Config:
-    SECRET_KEY = os.environ.get('SECRET_KEY')
+    SECRET_KEY = 'kdmlglmmgfhmfldmfdllhgghkkhgkd'
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or os.environ.get('SQLALCHEMY_DATABASE_URI')
     MAIL_SERVER = 'smtp.googlemail.com'
     MAIL_PORT = 587
