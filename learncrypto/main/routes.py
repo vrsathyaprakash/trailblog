a0edf977ef17def9d688b9e2b924ec9adbb02bf4
sathya <sathyaprakash777.sp@gmail.com>
2021-03-12 07:40:51 +0530

    modified_version

--- learncrypto/main/routes.py
+++ learncrypto/main/routes.py
@@ -1,11 +1,14 @@
 from flask import render_template, request, Blueprint
 from learncrypto.models import Post
+from flask_login import login_required
+
 
 main = Blueprint('main', __name__)
 
 
 @main.route("/")
 @main.route("/home")
+@login_required
 def home():
     page = request.args.get('page', 1, type=int)
     posts = Post.query.order_by(Post.date_posted.desc()).paginate(page=page, per_page=5)
